fd7e303fc3cbdec79ba850df68cd578cf5eb31e0
david <david@pop-os.localdomain>
2021-11-23 11:15:37 +0000

    add geolocation module

--- iot/App.tsx
+++ iot/App.tsx
@@ -1,8 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Geolocation from 'react-native-geolocation-service';
+import { PermissionsAndroid } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+
 export default function App() {
+
+  const [gps, setGps] = useState({});
+  const [permissions, setPermissions] = useState(false);
+
+
+
+
+  useEffect(async () => {
+    try {
+      const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION)
+      console.log(granted);
+    } finally {
+      
+    }
+    Geolocation.getCurrentPosition((position) => {
+      console.log(position);
+    },
+    (error) => {
+      console.log(error)
+    }
+
+    );
+  });
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
